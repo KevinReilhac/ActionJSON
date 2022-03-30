669dd4b27c294396caab47e8680df4468500daa8
Kévin Reilhac <kevin.reilhac.pro@gmail.com>
2022-03-30 15:53:51 +0200

    Add ActionJSON constructor & JsonConversion

--- Runtime/Action.cs
+++ Runtime/Action.cs
@@ -1,8 +1,26 @@
+using Newtonsoft.Json.Linq;
+
 namespace Hilo.ActionJSON
 {
 	public class ActionJSON
 	{
 		public string type;
 		public Payload payload;
+
+		public ActionJSON(string type, Payload payload)
+		{
+			this.type = type;
+			this.payload = payload;
+		}
+
+		public static string GetJSONFromPayload<T>(string type, T payload) where T : Payload
+		{
+			JObject json = new JObject();
+
+			json["type"] = type;
+			json["payload"] = JToken.FromObject(payload);
+
+			return (json.ToString());
+		}
 	}
 }
